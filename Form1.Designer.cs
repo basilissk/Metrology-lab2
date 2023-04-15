2cabe8af7e5c265dcae5fd19f904e0792e218573
basilissk <vanasilvergoldsssk@mail.ru>
2023-04-15 21:58:26 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.bAnalys = new System.Windows.Forms.Button();
+            this.tBSourceText = new System.Windows.Forms.TextBox();
+            this.bSelectFile = new System.Windows.Forms.Button();
+            this.table = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bAnalys
+            // 
+            this.bAnalys.Enabled = false;
+            this.bAnalys.Location = new System.Drawing.Point(118, 561);
+            this.bAnalys.Name = "bAnalys";
+            this.bAnalys.Size = new System.Drawing.Size(140, 23);
+            this.bAnalys.TabIndex = 0;
+            this.bAnalys.Text = "Анализировать";
+            this.bAnalys.UseVisualStyleBackColor = true;
+            this.bAnalys.Click += new System.EventHandler(this.bAnalys_Click);
+            // 
+            // tBSourceText
+            // 
+            this.tBSourceText.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tBSourceText.Location = new System.Drawing.Point(12, 12);
+            this.tBSourceText.Multiline = true;
+            this.tBSourceText.Name = "tBSourceText";
+            this.tBSourceText.ReadOnly = true;
+            this.tBSourceText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tBSourceText.Size = new System.Drawing.Size(384, 473);
+            this.tBSourceText.TabIndex = 1;
+            // 
+            // bSelectFile
+            // 
+            this.bSelectFile.Location = new System.Drawing.Point(118, 514);
+            this.bSelectFile.Name = "bSelectFile";
+            this.bSelectFile.Size = new System.Drawing.Size(140, 23);
+            this.bSelectFile.TabIndex = 3;
+            this.bSelectFile.Text = "Выбрать файл";
+            this.bSelectFile.UseVisualStyleBackColor = true;
+            this.bSelectFile.Click += new System.EventHandler(this.bSelectFile_Click);
+            // 
+            // table
+            // 
+            this.table.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.table.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.table.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.table.Location = new System.Drawing.Point(443, 12);
+            this.table.Name = "table";
+            this.table.ReadOnly = true;
+            this.table.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.table.Size = new System.Drawing.Size(753, 473);
+            this.table.TabIndex = 5;
+            // 
+            // Column1
+            // 
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Column1.HeaderText = "i";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 75;
+            // 
+            // Column2
+            // 
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Column2.HeaderText = "Оператор";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 200;
+            // 
+            // Column3
+            // 
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Column3.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Column3.HeaderText = "f1i";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 75;
+            // 
+            // Column4
+            // 
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Column4.DefaultCellStyle = dataGridViewCellStyle4;
+            this.Column4.HeaderText = "j";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 75;
+            // 
+            // Column5
+            // 
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Column5.DefaultCellStyle = dataGridViewCellStyle5;
+            this.Column5.HeaderText = "Операнд";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 200;
+            // 
+            // Column6
+            // 
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Column6.DefaultCellStyle = dataGridViewCellStyle6;
+            this.Column6.HeaderText = "f2j";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 75;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(443, 514);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(742, 70);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1208, 632);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.table);
+            this.Controls.Add(this.bSelectFile);
+            this.Controls.Add(this.tBSourceText);
+            this.Controls.Add(this.bAnalys);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bAnalys;
+        private System.Windows.Forms.TextBox tBSourceText;
+        private System.Windows.Forms.Button bSelectFile;
+        private System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        MatchCollection conditionalOperatorsMatches;
+        MatchCollection operatorsMatches;
+        MatchCollection switchMathes;
+
+        Dictionary<string, int> operatorsHash = new Dictionary<string, int>();
+        Dictionary<string, int> conditionalOperatorsHash = new Dictionary<string, int>();
+
+        string code = "";
+        int operatorsCount = 0;
+        int conditionalOperatorsCount = 0;
+        int maxMastedLevel = 0;
+      
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFileDialog.InitialDirectory = Environment.CurrentDirectory;
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.FilterIndex = 2;
+
+        }
+
+        static string DeleteComments(string text)
+        {
+            return Regex.Replace(text, "/\\*.*?\\*/|//.*\r\n", "");
+        }
+
+        static string DeleteIncludes(string text)
+        {
+            return Regex.Replace(text, "#include.*\r?\n", "");
+        }
+
+        static string DeleteUsing(string text)
+        {
+            return Regex.Replace(text, "using.*\r?\n", "");
+        }
+
+        static void AddToHash(Match match, Dictionary<string, int> HashMap)
+        {
+            HashMap[match.Value] = HashMap.ContainsKey(match.Value) ? HashMap[match.Value] + 1 : 1;
+        }
+
+        void Analys()
+        {
+            Regex switchReg = new Regex("switch");
+            Regex operators = new Regex(@"!=|==|=");
+            Regex conditionalOperators = new Regex("else if|if|else|case|while|for");
+
+            code = DeleteComments(code);
+            code = DeleteIncludes(code);
+            code = DeleteUsing(code);
+
+            operatorsMatches = operators.Matches(code);
+            conditionalOperatorsMatches = conditionalOperators.Matches(code);
+            switchMathes = switchReg.Matches(code);
+
+            operatorsHash.Clear();
+            conditionalOperatorsHash.Clear();
+            
+         
+            foreach (Match match in operatorsMatches)
+            {
+                AddToHash(match, operatorsHash);
+            }
+
+            foreach (Match match in conditionalOperatorsMatches)
+            {
+                AddToHash(match, conditionalOperatorsHash);
+            }
+
+        }
+
+        private void bAnalys_Click(object sender, EventArgs e)
+        {
+
+            int rowCount = 0;
+            int rowInd = 0;
+
+            Analys();
+
+            rowCount = operatorsHash.Count + conditionalOperatorsHash.Count;
+            rowCount++;
+            table.RowCount = rowCount;
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                table[0, i].Value = i + 1 + ".";
+                table[3, i].Value = i + 1 + ".";
+            }
+
+            foreach (KeyValuePair<string, int> kvp in operatorsHash)
+            {
+                table[1, rowInd].Value = kvp.Key;
+                table[2, rowInd].Value = kvp.Value;
+                rowInd++;
+            }
+
+            foreach (KeyValuePair<string, int> kvp in conditionalOperatorsHash)
+            {
+                table[1, rowInd].Value = kvp.Key;
+                table[2, rowInd].Value = kvp.Value;
+
+                rowInd++;
+            }
+
+            rowInd = 0;
+
+            rowInd = table.RowCount - 1;
+
+            table[0, rowInd].Value = $"n1 = {operatorsHash.Count}";
+            table[2, rowInd].Value = $"N1 = {operatorsMatches.Count}";
+           
+            operatorsCount = GetOperatorsAmount(operatorsHash,operatorsMatches);
+            conditionalOperatorsCount = conditionalOperatorsMatches.Count - switchMathes.Count;
+            maxMastedLevel = CalculateMaxNestedLevel(code);
+            textBox1.Text = $"CL = {conditionalOperatorsCount}\r\n" +
+                $"cl = {((float)conditionalOperatorsCount / (operatorsCount + conditionalOperatorsCount)):F2}\r\n" +
+                $"CLI = {maxMastedLevel}\r\n";
+        }
+
+        private void bSelectFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                var fileStream = openFileDialog.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    code = reader.ReadToEnd();
+                }
+
+                bAnalys.Enabled = true;
+
+                tBSourceText.Text = code;
+
+            }
+            else
+            {
+                bAnalys.Enabled = false;
+            }
+        }
+
+        static int GetOperatorsAmount(Dictionary<string, int> hash, MatchCollection match)
+        {
+            int amount = 0;
+            
+            foreach (KeyValuePair<string, int> kvp in hash)
+            {
+                if (kvp.Key == "==" || kvp.Key == "!=") amount+=kvp.Value;
+            }
+            return match.Count - amount;
+        }
+
+        static int CalculateMaxNestedLevel(string code)
+        {
+            int maxLevel = 0;
+            int currentLevel = 0;
+            string[] lines = code.Split('\n');
+            foreach (string line in lines)
+            {
+                if (line.Contains("if") || line.Contains("else") || line.Contains("switch") || line.Contains("case"))
+                {
+                    if (line.Contains("{"))
+                    {
+                        currentLevel++;
+                        if (currentLevel > maxLevel)
+                        {
+                            maxLevel = currentLevel;
+                        }
+                    }
+                    else if (line.Contains("}"))
+                    {
+                        currentLevel--;
+                    }
+                }
+            }
+            return maxLevel;
+        }
+
+    }
+}
